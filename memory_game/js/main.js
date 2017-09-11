8104a66f8dfe86c60356d8e7d2e7a3eb97223e17
emmadema <emma.demango@gmail.com>
2017-09-10 21:30:27 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,7 +24,6 @@ var cards = [
 ];
 
 var cardsInPlay = [];
-
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -33,18 +32,29 @@ var checkForMatch = function () {
 	}
 };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-
-	console.log("User flipped" + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);	
-
 	if(cardsInPlay.length === 2) {
 		checkForMatch();
 	}
-	
+	console.log("User flipped" + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);	
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function (){
+	for (var i=0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
+
+
